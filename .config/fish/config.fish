949b3b0ae018cb20525d0a1997fe0fbabbcbb704
AesaKamar <aesa.kamar@gmail.com>
2018-04-30 08:48:45 -0400

    Hack to pull some nix configs into fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,10 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 set PATH /usr/local/arcanist/bin $PATH
 set PATH /Users/aesa/.local/bin $PATH
 set PATH ~/.npm-global/bin $PATH
+
+
+# Nix
+if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+end
+# End Nix
