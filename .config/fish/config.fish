aa6edf8072455edca10f5a3250f3eb985c547051
AesaKamar <aesa.kamar@gmail.com>
2019-01-08 09:23:59 -0500

    sbt memory allocation defaults increase

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,3 +35,6 @@ function wgetAll
     --no-parent \
       $argv
 end
+
+#SBT
+export SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M  -Duser.timezone=GMT"
